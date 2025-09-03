963abd1d2851cd64bf9e21b02c1373c3df817637
Abanob Essam <abanobessam19@gmail.com>
2025-09-04 02:16:55 +0300

    feat: adding footer

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -2,6 +2,7 @@ import Header from "./Header";
 import Sidebar from "./Sidebar";
 import { Outlet } from "react-router-dom";
 import "../assets/css/main.css";
+import Footer from "./Footer";
 
 function Layout() {
   return (
@@ -9,8 +10,11 @@ function Layout() {
       <Header />
       <div className="main-container">
         <Sidebar />
-        <div className="content">
-          <Outlet />
+        <div className="content-container">
+          <div className="content">
+            <Outlet />
+          </div>
+          <Footer />
         </div>
       </div>
     </div>
